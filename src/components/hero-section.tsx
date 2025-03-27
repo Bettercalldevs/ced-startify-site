e182be3b0287723405e2113d0f6360bbe5d5712c
BcdGenew <bcd.genewtech@gmail.com>
2025-03-27 11:14:34 +0530

    Update hero-section.tsx

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -180,9 +180,9 @@ export default function HeroSection() {
             </motion.p>
             <motion.div variants={fadeInUp} className="flex gap-4 mt-6">
               <GradientButton
-                  onClick={() => setDetailsOpen(true)} // Open the dialog
-                  className="w-[30%]"
-                  label="Event Registration"
+                href="#about-card"
+                className="w-[30%]"
+                label="Event Registration"
               />
 
             </motion.div>
