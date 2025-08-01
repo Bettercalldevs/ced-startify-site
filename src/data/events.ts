5f940cbcd1b13a1215bdea3e49cecdbb87b995e3
Cibisuryaa <148597929+Seradeep@users.noreply.github.com>
2025-08-01 17:30:16 +0530

    image update

--- src/data/events.ts
+++ src/data/events.ts
@@ -33,7 +33,7 @@ export const events = [
       "To recognize and celebrate the entrepreneurial spirit within academia. This award will highlight successful faculty-led startups that have made significant contributions to innovation and societal impact.",
     prizeAmount: "TBD",
     regFee: "6250",
-    imageSrc: "/images/",
+    imageSrc: "/images/faculty_pitch.webp",
   },
   {
     id: "intern-hunt",
